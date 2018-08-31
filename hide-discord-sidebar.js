5ca1a538543e1807a1e77ca3b4554e2313f99bcb
Patrick Chong <pcjh@umich.edu>
2018-08-31 15:12:51 -0400

    Version 1.1 complete: Sidebar now minimizes automatically when browser window is small. Moved Hide/Show button to top right.

--- hide-discord-sidebar.js
+++ hide-discord-sidebar.js
@@ -1,5 +1,5 @@
 function discordHack() {
-  if (document.querySelector('.divider-2PMBlV').parentNode.childNodes.length == 7 || document.querySelector('.divider-2PMBlV').parentNode.childNodes.length == 4 ) {
+  if (document.querySelector('.divider-2PMBlV').parentNode.childNodes.length == 7 || document.querySelector('.divider-2PMBlV').parentNode.childNodes.length == 4) {
 
     var btn = document.createElement("BUTTON");
     var t = document.createTextNode("Hide/Show");
@@ -13,12 +13,23 @@ function discordHack() {
         document.getElementsByClassName('channels-Ie2l6A')[0].style.width = '35vmin';
       } else {
         document.getElementsByClassName('guildsWrapper-5TJh6A')[0].style.display = 'none';
-        document.getElementsByClassName('channels-Ie2l6A')[0].style.width = '30vmin';
+        document.getElementsByClassName('channels-Ie2l6A')[0].style.width = '25vmin';
       }
     }
-    document.querySelector(".channels-Ie2l6A").insertBefore(btn,document.querySelector(".channels-Ie2l6A").firstChild)
+    document.body.appendChild(btn)
     console.log("Discord Sidebar Hack Complete!");
   }
 }
+
+window.onresize = function () {
+  if (window.innerWidth < 700) {
+    document.getElementsByClassName('guildsWrapper-5TJh6A')[0].style.display = 'none';
+    document.getElementsByClassName('channels-Ie2l6A')[0].style.width = '25vmin';
+  } else {
+    document.getElementsByClassName('guildsWrapper-5TJh6A')[0].style.display = 'flex';
+    document.getElementsByClassName('channels-Ie2l6A')[0].style.width = '35vmin';
+  }
+}
+
 
 setTimeout(discordHack, 2000);
