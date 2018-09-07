2e44332338f2932aa70dade8790d3c63dbecacb1
Patrick Chong <pcjh@umich.edu>
2018-09-06 20:44:26 -0400

    V2 done: Hide/Show btn for server bar. Unfolding sidebar for channels.

--- hide-discord-sidebar.js
+++ hide-discord-sidebar.js
@@ -1,35 +1,66 @@
 function discordHack() {
+
+  // When Discord chat is loaded
   if (document.querySelector('.divider-2PMBlV').parentNode.childNodes.length == 7 || document.querySelector('.divider-2PMBlV').parentNode.childNodes.length == 4) {
+    var channelPlaceholder = "3.5vmin";
+    var channelWidth = "40vmin";
+    var channels = document.getElementsByClassName('channels-Ie2l6A')[0];
+    
+    channels.style.width = 0;
+    channels.style.paddingLeft = channelPlaceholder;
+    
+    channels.addEventListener("mouseover", function () {
+      channels.style.width = channelWidth;
+      channels.style.paddingLeft = 0;
+    })
+    
+    channels.addEventListener("mouseout", function () {
+      channels.style.width = 0;
+      channels.style.paddingLeft = channelPlaceholder;
+    })
+    
+    var guildsWrapper = document.getElementsByClassName('guildsWrapper-5TJh6A')[0];
+
+    if (window.innerWidth < 700) {
+      hideServers();
+    }
+
+    window.onresize = function () {
+      if (window.innerWidth < 700) {
+        hideServers();
+      } else {
+        showServers();
+      }
+    }
 
     var btn = document.createElement("BUTTON");
-    var t = document.createTextNode("Hide/Show");
+    var t = document.createTextNode("Hide Servers");
     btn.appendChild(t);
-    btn.classList.add("hideNshow");
+    btn.id = "hideNshow";
 
     btn.onclick = function () {
       if (
-        document.getElementsByClassName('guildsWrapper-5TJh6A')[0].style.display === 'none') {
-        document.getElementsByClassName('guildsWrapper-5TJh6A')[0].style.display = 'flex';
-        document.getElementsByClassName('channels-Ie2l6A')[0].style.width = '35vmin';
+        guildsWrapper.style.display === 'none') {
+        showServers();
       } else {
-        document.getElementsByClassName('guildsWrapper-5TJh6A')[0].style.display = 'none';
-        document.getElementsByClassName('channels-Ie2l6A')[0].style.width = '25vmin';
+        hideServers();
       }
     }
     document.body.appendChild(btn)
+
+    
     console.log("Discord Sidebar Hack Complete!");
+
+    function hideServers() {
+      guildsWrapper.style.display = 'none';
+      btn.innerHTML = "Show Servers"
+    }
+  
+    function showServers() {
+      guildsWrapper.style.display = 'flex';
+      btn.innerHTML = "Hide Servers"
+    }
   }
 }
-
-window.onresize = function () {
-  if (window.innerWidth < 700) {
-    document.getElementsByClassName('guildsWrapper-5TJh6A')[0].style.display = 'none';
-    document.getElementsByClassName('channels-Ie2l6A')[0].style.width = '25vmin';
-  } else {
-    document.getElementsByClassName('guildsWrapper-5TJh6A')[0].style.display = 'flex';
-    document.getElementsByClassName('channels-Ie2l6A')[0].style.width = '35vmin';
-  }
-}
-
 
 setTimeout(discordHack, 2000);
