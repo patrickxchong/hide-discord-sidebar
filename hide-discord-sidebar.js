bd5bd958843e8f93d04a2bf6cfec4694c4859085
Patrick Chong <pcjh@umich.edu>
2018-09-06 21:40:19 -0400

    V2.1: minor optimization improvements

--- hide-discord-sidebar.js
+++ hide-discord-sidebar.js
@@ -2,36 +2,28 @@ function discordHack() {
 
   // When Discord chat is loaded
   if (document.querySelector('.divider-2PMBlV').parentNode.childNodes.length == 7 || document.querySelector('.divider-2PMBlV').parentNode.childNodes.length == 4) {
+    clearInterval(hider);
+
+    /* CHANNELS */
     var channelPlaceholder = "3.5vmin";
     var channelWidth = "40vmin";
     var channels = document.getElementsByClassName('channels-Ie2l6A')[0];
-    
+
     channels.style.width = 0;
     channels.style.paddingLeft = channelPlaceholder;
-    
+
     channels.addEventListener("mouseover", function () {
       channels.style.width = channelWidth;
       channels.style.paddingLeft = 0;
     })
-    
+
     channels.addEventListener("mouseout", function () {
       channels.style.width = 0;
       channels.style.paddingLeft = channelPlaceholder;
     })
-    
+
+    /* SERVERS */
     var guildsWrapper = document.getElementsByClassName('guildsWrapper-5TJh6A')[0];
-
-    if (window.innerWidth < 700) {
-      hideServers();
-    }
-
-    window.onresize = function () {
-      if (window.innerWidth < 700) {
-        hideServers();
-      } else {
-        showServers();
-      }
-    }
 
     var btn = document.createElement("BUTTON");
     var t = document.createTextNode("Hide Servers");
@@ -47,20 +39,33 @@ function discordHack() {
       }
     }
     document.body.appendChild(btn)
-
     
-    console.log("Discord Sidebar Hack Complete!");
+
+
+    if (window.innerWidth < 700) {
+      hideServers();
+    }
+
+    window.onresize = function () {
+      if (window.innerWidth < 700) {
+        hideServers();
+      } else {
+        showServers();
+      }
+    }
 
     function hideServers() {
       guildsWrapper.style.display = 'none';
       btn.innerHTML = "Show Servers"
     }
-  
+
     function showServers() {
       guildsWrapper.style.display = 'flex';
       btn.innerHTML = "Hide Servers"
     }
+
+    console.log("Discord Sidebar Hack Complete!");
   }
 }
 
-setTimeout(discordHack, 2000);
+var hider = setInterval(discordHack, 2000);
