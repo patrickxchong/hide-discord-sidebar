18f20d07da6f8dadcf07c3242330110df8229679
Bunnydreams <pcjh@umich.edu>
2019-01-20 14:36:46 -0500

    v2.5 testing phase with updated script that only runs when document is complete and moved sidebar logic to css

--- hide-discord-sidebar.js
+++ hide-discord-sidebar.js
@@ -1,71 +1,91 @@
 function discordHack() {
 
   // When Discord chat is loaded
-  if (document.querySelector('.divider-2PMBlV').parentNode.childNodes.length == 7 || document.querySelector('.divider-2PMBlV').parentNode.childNodes.length == 4) {
-    clearInterval(hider);
+  // if (document.querySelector('.divider-2PMBlV').parentNode.childNodes.length == 7 || document.querySelector('.divider-2PMBlV').parentNode.childNodes.length == 4) {
 
-    /* CHANNELS */
-    var channelPlaceholder = "3.5vmin";
-    var channelWidth = "40vmin";
-    var channels = document.getElementsByClassName('channels-Ie2l6A')[0];
+  /* CHANNELS */
+  // var channelPlaceholder = "3.5vmin";
+  // var channelWidth = "40vmin";
+  // var channels = document.getElementsByClassName('channels-Ie2l6A')[0];
 
-    channels.style.width = 0;
-    channels.style.paddingLeft = channelPlaceholder;
+  // channels.style.width = 0;
+  // channels.style.paddingLeft = channelPlaceholder;
 
-    channels.addEventListener("mouseover", function () {
-      channels.style.width = channelWidth;
-      channels.style.paddingLeft = 0;
-    })
+  // channels.addEventListener("mouseover", function() {
+  //   channels.style.width = channelWidth;
+  //   channels.style.paddingLeft = 0;
+  // })
 
-    channels.addEventListener("mouseout", function () {
-      channels.style.width = 0;
-      channels.style.paddingLeft = channelPlaceholder;
-    })
+  // channels.addEventListener("mouseout", function() {
+  //   channels.style.width = 0;
+  //   channels.style.paddingLeft = channelPlaceholder;
+  // })
 
-    /* SERVERS */
-    var guildsWrapper = document.getElementsByClassName('guildsWrapper-5TJh6A')[0];
+  /* SERVERS */
+  var guildsWrapper = document.getElementsByClassName('guildsWrapper-5TJh6A')[0];
 
-    var btn = document.createElement("BUTTON");
-    var t = document.createTextNode("Hide Servers");
-    btn.appendChild(t);
-    btn.id = "hideNshow";
+  var btn = document.createElement("BUTTON");
+  var t = document.createTextNode("Hide Servers");
+  btn.appendChild(t);
+  btn.id = "hideNshow";
 
-    btn.onclick = function () {
-      if (
-        guildsWrapper.style.display === 'none') {
-        showServers();
-      } else {
-        hideServers();
-      }
-    }
-    document.body.appendChild(btn)
-    
-
-
-    if (window.innerWidth < 700) {
+  btn.onclick = function() {
+    if (
+      guildsWrapper.style.display === 'none') {
+      showServers();
+    } else {
       hideServers();
     }
-
-    window.onresize = function () {
-      if (window.innerWidth < 700) {
-        hideServers();
-      } else {
-        showServers();
-      }
-    }
-
-    function hideServers() {
-      guildsWrapper.style.display = 'none';
-      btn.innerHTML = "Show Servers"
-    }
-
-    function showServers() {
-      guildsWrapper.style.display = 'flex';
-      btn.innerHTML = "Hide Servers"
-    }
-
-    console.log("Discord Sidebar Hack Complete!");
   }
+  document.body.appendChild(btn)
+
+
+
+  if (window.innerWidth < 700) {
+    hideServers();
+  }
+
+  window.onresize = function() {
+    if (window.innerWidth < 700) {
+      hideServers();
+    } else {
+      showServers();
+    }
+  }
+
+  function hideServers() {
+    guildsWrapper.style.display = 'none';
+    btn.innerHTML = "Show Servers"
+  }
+
+  function showServers() {
+    guildsWrapper.style.display = 'flex';
+    btn.innerHTML = "Hide Servers"
+  }
+
+  var styles = [
+    'background: linear-gradient(#D33106, #571402)'
+    , 'border: 1px solid #3E0E02'
+    , 'color: white'
+    , 'display: block'
+    , 'text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)'
+    , 'background-image: linear-gradient(180deg, #667eea 0%, #764ba2 100%)',
+    , 'line-height: 40px'
+    , 'text-align: center'
+    , 'font-weight: bold'
+    , 'font-size: 18px'
+  ].join(';');
+
+  console.log('%c Hide Discord Sidebar extension activated ', styles);
+  // }
 }
 
-var hider = setInterval(discordHack, 2000);
+// var hider = setInterval(discordHack, 2000);
+// document.addEventListener('DOMContentLoaded', discordHack, false);
+// alternative to DOMContentLoaded
+document.onreadystatechange = function() {
+  console.log(document.readyState);
+  if (document.readyState === "complete") {
+    discordHack();
+  }
+}
