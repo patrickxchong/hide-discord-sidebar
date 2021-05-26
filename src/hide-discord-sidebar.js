a897f70766abe7464dd1496215210f68ff3ce234
Patrick Chong <patrickxchong@gmail.com>
2021-05-26 11:15:07 +0800

    feat: enable/disable hide/autohide on Discord UI based on configuration

--- src/hide-discord-sidebar.js
+++ src/hide-discord-sidebar.js
@@ -1,15 +1,18 @@
 function hideDiscordSidebar() {
-  /* SERVERS */
+  // initialise default state
+  let state = {
+    active: false,
+    channels: "channel-disable",
+    servers: "server-disable",
+    smallWindowWidth: "700",
+  };
 
   // Select the server list element, which changes in some updates. 
   const guildsWrapper = document.getElementsByClassName('guildsWrapper-5TJh6A')[0]
     || document.getElementsByClassName('wrapper-1Rf91z')[0]
     || document.getElementsByClassName('wrapper-3NnKdC')[0]
-    // Experimenting with document.querySelector("nav[class*=wrapper-]") 
-    // as a general fallback to select the server list.
+    // General fallback to select the server list.
     || document.querySelector("nav[class*=wrapper-]");
-
-  // const app = document.getElementsByClassName('base-3dtUhz')[0];
 
   const btn = document.createElement("BUTTON");
   const t = document.createTextNode("Hide Servers");
@@ -28,17 +31,21 @@ function hideDiscordSidebar() {
   let timeout = false; // holder for timeout id
   window.addEventListener('resize', function () {
     clearTimeout(timeout);
-    // start timing for event "completion"
     timeout = setTimeout(resizeHandler, 250);
   });
 
   function resizeHandler() {
     // only if extension is active
-    if (document.body.classList.contains("hide-dis-bar")) {
-      if (window.innerWidth < 700) {
-        hideServers();
-      } else {
-        showServers();
+    if (state.active) {
+      if (state.servers == "server-autohide") {
+        if (window.innerWidth < state.smallWindowWidth) {
+          hideServers();
+        } else {
+          showServers();
+        }
+      }
+      if (state.channels == "channel-autohide") {
+        document.body.classList.toggle("channel-hide", window.innerWidth < state.smallWindowWidth);
       }
     }
   }
@@ -47,28 +54,24 @@ function hideDiscordSidebar() {
   chrome.runtime.sendMessage({ action: "initialize" });
 
   chrome.runtime.onMessage.addListener(function (request) {
-    console.log(request);
-    if (request.active) {
-      document.body.classList.add("hide-dis-bar");
-      // hide servers by default when screen width is small
-      if (window.innerWidth < 700) {
-        hideServers();
-      }
-    } else {
-      document.body.classList.remove("hide-dis-bar");
-      showServers(); // force show servers list (if it's hidden) when extension is disabled
+    state = request;
+    console.log(state);
+
+    document.body.classList.toggle("hide-dis-bar", state.active);
+    document.body.classList.toggle("channel-hide", state.channels == "channel-hide");
+    if (state.servers = "server-disable") {
+      hideServers();
     }
+    resizeHandler();
   });
 
   function hideServers() {
     guildsWrapper.style.display = 'none';
-    // app.style.left = 0;
     btn.innerHTML = "Show Servers";
   }
 
   function showServers() {
     guildsWrapper.style.display = 'flex';
-    // app.style.left = "72px";
     btn.innerHTML = "Hide Servers";
   }
 
@@ -88,13 +91,13 @@ function hideDiscordSidebar() {
   console.log('%c Hide Discord Sidebar extension activated ', styles);
 }
 
-// document.addEventListener('DOMContentLoaded', hideDiscordSidebar, false);
-// alternative to DOMContentLoaded
-document.onreadystatechange = function () {
+// DOMContentLoaded and load events don't work 
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/readystatechange_event
+document.addEventListener('readystatechange', function () {
   if (document.readyState === "complete") {
     // Check that the Discord page contains the base chat element
     if (document.getElementsByClassName('base-3dtUhz')[0]) {
       hideDiscordSidebar();
     }
   }
-};
+});
