5afec9da56cebcb50c1e1470b440dc4303a809ce
Patrick Chong <patrickxchong@gmail.com>
2022-01-04 21:09:38 +0800

    v3.2: update code to use Vanilla javascript for Firefox support

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -9,7 +9,7 @@ function getState(query) {
 
 function sendMessage(message) {
   return new Promise((resolve, reject) => {
-    message = JSON.parse(JSON.stringify(message)) // prevent DataCloneError
+    message = JSON.parse(JSON.stringify(message)); // prevent DataCloneError
     chrome.runtime.sendMessage(message, function (result) {
       resolve(result);
     });
@@ -26,8 +26,6 @@ function executeScript(script) {
   });
 }
 
-MicroModal.init();
-
 function ComponentHDS() {
   return {
     state: {
@@ -39,31 +37,77 @@ function ComponentHDS() {
     inputWindowWidth: 700,
     parentWindowWidth: 0,
     async created() {
-      let state = await getState(null);
-      console.log(state);
-      this.state = state;
+      try {
+        let state = await getState(null);
+        console.log(state);
+        this.state = state;
+        document.getElementById(this.state.servers).checked = true;
+        document.getElementById(this.state.channels).checked = true;
+      } catch (e) { }
+
       this.inputWindowWidth = this.state.smallWindowWidth;
-      this.$watch('state.servers', async value => {
-        await sendMessage({ action: "update", state: this.state });
+      this.toggleActive(this.state.active);
+
+      let root = this;
+      document.querySelector(".button-power").addEventListener("click", () => root.toggleActive());
+      document.querySelector(".button-save").addEventListener("click", () => root.saveOptions());
+
+      document.querySelectorAll(".icon-gear").forEach(item => {
+        item.addEventListener("click", () => root.openOptionsModal());
       });
-      this.$watch('state.channels', async value => {
-        await sendMessage({ action: "update", state: this.state });
+      document.querySelectorAll("[id*=server]").forEach(item => {
+        item.addEventListener("click", async function () {
+          root.state.servers = this.value;
+          await sendMessage({ action: "update", state: root.state });
+        });
+      });
+      document.querySelectorAll("[id*=channel]").forEach(item => {
+        item.addEventListener("click", async function () {
+          root.state.channels = this.value;
+          await sendMessage({ action: "update", state: root.state });
+        });
       });
     },
-    async toggleActive() {
-      this.state.active = !this.state.active;
+    async toggleActive(placeholder = 10) {
+      // set placeholder value so that active is only toggled when no argument is provided
+      if (placeholder === 10) {
+        this.state.active = !this.state.active;
+      }
+      let buttonPower = document.querySelector(".button-power");
+      if (this.state.active) {
+        buttonPower.removeAttribute('inactive');
+        document.querySelector(".button-power-text").innerText = "Enabled";
+      } else {
+        buttonPower.setAttribute('inactive', 'inactive');
+        document.querySelector(".button-power-text").innerText = "Disabled";
+      }
+      await sendMessage({
+        action: "update", state: this.state
+      });
+    },
+    async toggleServers(value) {
+      this.state.servers = value;
       await sendMessage({ action: "update", state: this.state });
+    },
+    async toggleChannels(value) {
+      this.state.channels = value;
+      await sendMessage({ action: "update", state: this.state });
+
     },
     async openOptionsModal() {
       let results = await executeScript("window.innerWidth");
-      this.parentWindowWidth = results[0];
-      this.inputWindowWidth = this.state.smallWindowWidth;
+      document.getElementById("parentWindowWidth").innerText = results[0];
+      document.getElementById("inputWindowWidth").value = this.state.smallWindowWidth;
       MicroModal.show("options-modal");
     },
     async saveOptions() {
-      this.state.smallWindowWidth = this.inputWindowWidth;
+      this.state.smallWindowWidth = document.getElementById("inputWindowWidth").value;
       await sendMessage({ action: "update", state: this.state });
       MicroModal.close("options-modal");
     }
   };
 }
+
+MicroModal.init();
+window.hds = ComponentHDS();
+hds.created();
